a3d1f7ec7cc2b8bac41b2d8c796ab58f9e0f8c2e
[Arthur] <115912843+Artthur0@users.noreply.github.com>
2024-11-06 22:39:24 -0300

    Mejoras de en menu de pausa

--- app/src/main/java/com/example/cronogame/components/PauseMenu.kt
+++ app/src/main/java/com/example/cronogame/components/PauseMenu.kt
@@ -2,7 +2,7 @@ package com.example.cronogame.components
 
 import android.app.AlertDialog
 import android.app.Dialog
-import android.app.DialogFragment
+import androidx.fragment.app.DialogFragment
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
