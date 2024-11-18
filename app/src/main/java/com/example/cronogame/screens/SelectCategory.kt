39fda2b71434bc39d871e6ec005067b90813986f
Seba Barra <sebab.s.002@gmail.com>
2024-11-18 12:08:58 -0300

    intento de SelectCategory.kt

--- app/src/main/java/com/example/cronogame/screens/SelectCategory.kt
+++ app/src/main/java/com/example/cronogame/screens/SelectCategory.kt
@@ -1,66 +1,139 @@
 package com.example.cronogame.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.room.Room
 import com.example.cronogame.components.Category
 import com.example.cronogame.components.TopBar
+import com.example.cronogame.database.AppDatabase
+import com.example.cronogame.database.entities.Category
 import com.example.cronogame.navigation.AppScreens
 
 
 @Composable
+@OptIn(ExperimentalMaterial3Api::class)
 fun SelectCategory(navController: NavController) {
-    val categories =
-        listOf("Categoría 1", "Categoría 2", "Categoría 3", "Categoría 4", "Categoría 5")
-    Scaffold(
-        topBar = {
-            TopBar(
-                title = "Selecciona una categoría",
-                buttonIcon = Icons.Default.ArrowBack,
-                onButtonClick = { },
-                backgroundColor = Color(0xff73459f),
-                iconColor = Color.White
-            )
-        },
-        containerColor = Color(0xff73459f)
-    ) { innerPadding ->
+    val context = LocalContext.current
+    val database = remember {
+        Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "historical_events_db"
+        ).build()
+    }
 
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(innerPadding)
-                .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
-        ) {
-            items(categories) { category ->
-                Category(
-                    text = category,
-                    onClick = { navController.navigate(AppScreens.GameScreen.route)},
-                    backgroundColor = Color(0xffFFC900),
-                    textColor = Color.White
+    var isLoading by remember { mutableStateOf(true) }
+    var categories by remember { mutableStateOf(listOf<Category>()) }
 
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-
-
-            }
+    // Cargar categorías desde la base de datos
+    LaunchedEffect(Unit) {
+        try {
+            categories = database.categoryDao().getAllCategories()
+        } catch (e: Exception) {
+            categories = emptyList()
+        } finally {
+            isLoading = false
         }
+    }
 
+    if (isLoading) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    } else {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "Selecciona una Categoría",
+                                color = MaterialTheme.colorScheme.onBackground
+                            )
+                        }
+                    }
+                )
+            },
+            content = { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    if (categories.isEmpty()) {
+                        Text("No hay categorías disponibles.", style = MaterialTheme.typography.titleMedium)
+                    } else {
+                        Column(
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            categories.forEach { category ->
+                                Button(
+                                    onClick = {
+                                        navController.navigate(
+                                            "game_screen/${category.id}"
+                                        )
+                                    },
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .height(48.dp)
+                                ) {
+                                    Text(text = category.name, style = MaterialTheme.typography.bodyLarge)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        )
     }
 }
 /*
